74ca423795e8582aaa26f8b1c6fa8f1ccd1b9461
Dustin Swecker <dustin.swecker@gmail.com>
2023-05-15 21:32:08 -0400

    added assocations

--- models/index.js
+++ models/index.js
@@ -1,4 +1,34 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
 
-module.exports = { User, Blog }; 
+User.hasMany(Blog, {
+    foreignKey: 'creator_username',
+});
+
+Blog.belongsTo(User, {
+    foreignKey: 'creator_username',
+    onDelete: 'cascade'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'commenters_id',
+    onDelete: 'cascade'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'commenters_id',
+    onDelete: 'cascade'
+});
+
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+    onDelete: 'cascade'
+});
+
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id',
+    onDelete: 'cascade'
+});
+
+module.exports = { User, Blog, Comment }; 
