b1d2ef4ac74d99f62e2698ff3ded3b152419e16d
Dustin Swecker <dustin.swecker@gmail.com>
2023-05-15 21:23:23 -0400

    comments model added

---
+++ models/Comment.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('..config/connection');
+
+class Comment extends Model {}
+
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        commenters_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        blog_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: {
+                model: 'blog',
+                key: 'id',
+            },
+    },
+
+    },
+    { 
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blog',
+      }
+);
+
+module.exports = Comment;
